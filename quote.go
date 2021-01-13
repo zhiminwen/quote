b570b468a4f7ce636fcad3bae2d979e213954657
Zhimin <wenzm@sg.ibm.com>
2021-01-13 23:24:18 +0800

    don't split newline with \ infront

--- quote.go
+++ quote.go
@@ -20,7 +20,8 @@ func Word(line string) []string {
 // Line return the lines as a string slice, ignoring empty line, line as comment (started with // or #)
 func Line(lines string) []string {
 	var result []string
-	for _, l := range regexp.MustCompile(`\n`).Split(lines, -1) {
+	//if new line is ended with \, don't split it
+	for _, l := range regexp.MustCompile(`[^\\]\n`).Split(lines, -1) {
 		nl := strings.TrimSpace(l)
 		if nl == "" || strings.HasPrefix(nl, "//") || strings.HasPrefix(nl, "#") {
 			continue
